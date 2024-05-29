ec5d947baf4273748f088c36f9b0b3430a8d5557
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-05-29 16:19:56 +0500

    Add Whitespace_Impact.py

---
+++ Module 1 - Getting Started With Python/2. Welcome to Python Programming/Whitespace_Impact.py
@@ -0,0 +1,7 @@
+x = 1 +     2  #The whitespaces b/w + & 2 will cause no error
++ 5 #This will not be executed i.e no impact
+print(x)
+
+y=2\
++3 #using the backslash will continue previous statement
+print(y)
