86914878e08017781c6f78ebdb097967c90b2e89
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-05-29 16:19:29 +0500

    Add Python_Syntax.py

---
+++ Module 1 - Getting Started With Python/2. Welcome to Python Programming/Python_Syntax.py
@@ -0,0 +1,9 @@
+#The following code will result in an error:
+#print("Hello") print("World")
+
+#Solution 1: Use a semicolon
+print("Hello"); print("World")
+
+#Solution 2: Use a new line
+print("Hello")
+print("World")
