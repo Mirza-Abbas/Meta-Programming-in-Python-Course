5ee408627f4f83af8a9b0fee9b76b57d34dafb56
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-06-01 16:37:04 +0500

    Add Multiple_Variable_Assignment.py

---
+++ Module 1 - Getting Started With Python/2. Welcome to Python Programming/Multiple_Variable_Assignment.py
@@ -0,0 +1,13 @@
+#Assigning single value to multiple variables:
+a = b = c = 10
+
+print(a)
+print(b)
+print(c)
+
+#Assigning multiple values to multiple variables:
+a, b, c = 1, 2, 3
+
+print(a)
+print(b)
+print(c)
