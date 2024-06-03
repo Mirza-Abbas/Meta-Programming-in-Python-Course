6a769ab4a2969127b37259e2a06daa84d8125a7c
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-06-03 11:27:54 +0500

    Add Sequence_DataTypes.py

---
+++ Module 1 - Getting Started With Python/2. Welcome to Python Programming/Sequence_DataTypes.py
@@ -0,0 +1,18 @@
+#String Data Type:
+a = "Hello"
+
+print(type(a))
+
+#List Data Type:(mutable)
+b = [1, 0.1, "One"]
+
+print(b)
+print(b[2])
+print(type(b))
+
+#Tuples Data Type:(immutable)
+c = (1, 0.1, "One")
+
+print(c)
+print(c[1])
+print(type(c))
