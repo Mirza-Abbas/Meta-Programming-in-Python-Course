0adf0c8026f3a2122657f46569e9ed42b5013d2d
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-06-03 11:27:25 +0500

    Add Numeric_DataTypes.py

---
+++ Module 1 - Getting Started With Python/2. Welcome to Python Programming/Numeric_DataTypes.py
@@ -0,0 +1,18 @@
+#Integer Data Type:
+a = 10
+b = 0
+c = -10
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Float Data Type:
+d = 0.5
+e = -2.1
+print(type(d))
+print(type(e))
+
+#Complex Data Type:
+f = 10 + 3j
+print(type(f))
