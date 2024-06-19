8b2c87187d580ae9484af18713df228e8dfcccf9
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-06-19 18:36:30 +0500

    Add Not_Operator.py

---
+++ Module 1 - Getting Started With Python/3. Control Flow & Conditionals/Not_Operator.py
@@ -0,0 +1,13 @@
+a = True
+b = True
+
+if not(a) or not(b): #i.e not true or not true = false
+    print("Atleast on is True")
+
+print(a or b)
+
+a = False
+b = False
+
+if not(a) or not(b): #i.e not false or not false = True
+    print("Atleast on is True")
