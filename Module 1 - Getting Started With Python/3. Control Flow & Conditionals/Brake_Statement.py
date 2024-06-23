d2a238cffd998910b876a2add0391e2f19405ca8
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-06-24 00:01:11 +0500

    Add Brake_Statement.py

---
+++ Module 1 - Getting Started With Python/3. Control Flow & Conditionals/Brake_Statement.py
@@ -0,0 +1,8 @@
+favorites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramisú', 'Chocolate Cake']
+
+for dessert in favorites:
+    if dessert == 'Churros':
+        print('Yes one of my favorite desserts is', dessert)
+        break 
+    else:
+        print('No sorry, not a dessert on my list')
