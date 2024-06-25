7794ab8dd37c42078cce0e1638f945db570ff0b7
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-06-25 12:36:17 +0500

    Add Continue_Statement.py

---
+++ Module 1 - Getting Started With Python/3. Control Flow & Conditionals/Continue_Statement.py
@@ -0,0 +1,6 @@
+favorites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramisu', 'Chocolate Cake']
+
+for dessert in favorites:
+    if dessert == 'Churros':
+        continue
+    print('Other desserts I like are', dessert)
