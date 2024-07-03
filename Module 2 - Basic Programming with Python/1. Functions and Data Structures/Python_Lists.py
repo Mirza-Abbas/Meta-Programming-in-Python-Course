bf5c652f6a31ee24942a30908263074e786118ff
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-07-03 15:14:12 +0500

    Add Python_Lists.py

---
+++ Module 2 - Basic Programming with Python/1. Functions and Data Structures/Python_Lists.py
@@ -0,0 +1,15 @@
+list1 = [1, 2, 3, 4, 5]
+
+print(list1)
+
+list2 = ['A', 'B', 'C']
+
+print(list2)
+
+list3 = ["John", "Doe"]
+
+print(list3)
+
+list4 = [10, 'D', "Billy", True, 3.14]
+
+print(list4)
