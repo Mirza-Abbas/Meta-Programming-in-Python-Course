868d9510a7c72ead11a99917f0f72fc6ec4f74fe
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-08-03 12:52:08 +0500

    Add Procedural_Programming.py

---
+++ Module 3 - Programming Paradigms/1. Procedural Programming/Procedural_Programming.py
@@ -0,0 +1,15 @@
+#Using specific code:
+a = 2
+b = 3
+
+sum1 = a + b
+
+print(sum1)
+
+#instead use reusable methods:
+def add(a, b):
+    return a + b
+
+print(add(2, 3))
+print(add(4, 5))
+print(add(6, 7))
