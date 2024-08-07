be5f9afec9c4764660cb2ad524bdbdbe1c682b6a
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-08-07 20:22:30 +0500

    Add Factorial_Program.py

---
+++ Module 3 - Programming Paradigms/2. Functional Programming/Factorial_Program.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n < 0:
+        return 0
+    else:
+        factorial = 1
+        for i in range(1, n + 1):
+            factorial = factorial * i
+        return factorial
+    
+print(factorial(3))
