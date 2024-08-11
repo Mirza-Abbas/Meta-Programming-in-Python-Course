6f1de9bafd781bc8567f2ee192e967cfe2216138
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-08-11 15:31:46 +0500

    Add Filter_Function.py

---
+++ Module 3 - Programming Paradigms/2. Functional Programming/Filter_Function.py
@@ -0,0 +1,12 @@
+menu = ['espresso', 'mocha', 'latte', 'cappuccino', 'cortado', 'americano']
+
+def find_coffee(coffee):
+    if coffee[0] == 'c':
+        return coffee
+
+filter_coffee = filter(find_coffee, menu)
+
+print(filter_coffee)
+
+for x in filter_coffee:
+    print(x)
