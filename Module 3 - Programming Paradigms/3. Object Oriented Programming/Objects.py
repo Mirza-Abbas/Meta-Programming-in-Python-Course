7092a2fd69c9272271b50af525cd006899b8bc3f
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-08-15 21:17:50 +0500

    Add Objects.py

---
+++ Module 3 - Programming Paradigms/3. Object Oriented Programming/Objects.py
@@ -0,0 +1,14 @@
+class Employee:
+    #Contains attributes:
+    Position = ""
+    Employeement_Status = ""
+
+emp1 = Employee()
+
+emp1.Position = "Manager"
+emp1.Employeement_Status = "Full-Time"
+
+print(emp1.Position)
+print(emp1.Employeement_Status)
+print(emp1)
+print(type(emp1))
