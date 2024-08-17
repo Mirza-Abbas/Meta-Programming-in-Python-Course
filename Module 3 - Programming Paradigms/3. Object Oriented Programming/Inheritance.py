e78dfce1a22cba9d989669e8d686fccad44dbef3
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-08-17 16:33:14 +0500

    Add Inheritance.py

---
+++ Module 3 - Programming Paradigms/3. Object Oriented Programming/Inheritance.py
@@ -0,0 +1,11 @@
+#Inheritance Strucutre:
+
+class Parent:
+    #Members of Parent Class
+    pass
+
+class Child(Parent):
+    #Inherited members from Parent Class
+
+    #Additional members of Child Class
+    pass
