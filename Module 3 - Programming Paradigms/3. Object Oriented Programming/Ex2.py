7c5347d138ae9fc6d0ec081083f986831cc528f8
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-08-19 18:07:38 +0500

    Add Ex2.py

---
+++ Module 3 - Programming Paradigms/3. Object Oriented Programming/Ex2.py
@@ -0,0 +1,15 @@
+#Exercise: Instantiate a custom Object
+
+class MyFirstClass():
+    
+    print("Who wrote this?")
+
+    index = "Author-Book"
+    
+    def hand_list(self, philosopher, book):
+        print(self.index)
+        print(philosopher + "wrote the book: " + book)
+
+whodunnit = MyFirstClass()
+
+whodunnit.hand_list("Sun Tzu ", "The Art of War")
