70344e7a7c15bcb956db8cb3b734dad35422f947
Mirza Abbas <113203942+Mirza-Abbas@users.noreply.github.com>
2024-09-01 15:16:46 +0500

    Add Test_File.py, Importing_Files.py

---
+++ Module 4 - Modules, Packages, Libraries and Tools/1. Modules/Importing_Files.py
@@ -0,0 +1,5 @@
+import Test_File
+
+print(Test_File.Test_Var)
+
+print(Test_File.Test_Fn())
---
+++ Module 4 - Modules, Packages, Libraries and Tools/1. Modules/Test_File.py
@@ -0,0 +1,6 @@
+print("This is Test File")
+
+Test_Var = "This is Test Variable"
+
+def Test_Fn():
+    return "This is Test Function"
